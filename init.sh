be162ba4f4b06994ef5f1c1a485d70d97adcb3de
Adrian Carriger <hello@adriancarriger.com>
2018-06-18 20:32:08 -0500

    refactor(lambda): rename webhooks to lambda

--- init.sh
+++ init.sh
@@ -7,13 +7,13 @@ cd "$(dirname "$0")"
 if [[ $1 == "-f" ]]; then
   echo "Forcing a clean install…"
   rm -rf ../sl-data
-  rm -rf ../sl-webhooks
+  rm -rf ../sl-lambda
 fi
 
 if [[ ! -d ../sl-data ]]; then
   git clone --branch develop git@github.com:adriancarriger/sl-data.git ../sl-data
 fi
 
-if [[ ! -d ../sl-webhooks ]]; then
-  git clone --branch develop git@github.com:adriancarriger/sl-webhooks.git ../sl-webhooks
+if [[ ! -d ../sl-lambda ]]; then
+  git clone --branch develop git@github.com:adriancarriger/sl-lambda.git ../sl-lambda
 fi
