d1a0172cc729021ba8762b301d81c6f6b12a015c
Adrian Carriger <hello@adriancarriger.com>
2018-06-30 10:48:13 -0500

    feat(server): add graphql server

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ A docker-compose base repo for running serverless microservices locally
 
 - [sl-base](https://github.com/adriancarriger/sl-base)
 - [sl-data](https://github.com/adriancarriger/sl-data)
+- [sl-server](https://github.com/adriancarriger/sl-server)
 - [sl-lambda](https://github.com/adriancarriger/sl-lambda)
--- init.sh
+++ init.sh
@@ -14,6 +14,10 @@ if [[ ! -d ../sl-data ]]; then
   git clone --branch develop git@github.com:adriancarriger/sl-data.git ../sl-data
 fi
 
+if [[ ! -d ../sl-server ]]; then
+  git clone --branch develop git@github.com:adriancarriger/sl-server.git ../sl-server
+fi
+
 if [[ ! -d ../sl-lambda ]]; then
   git clone --branch develop git@github.com:adriancarriger/sl-lambda.git ../sl-lambda
 fi
