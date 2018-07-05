978f5eab321457a9bdc175597a074774c38ca33a
Adrian Carriger <hello@adriancarriger.com>
2018-07-05 15:13:04 -0500

    feat(sl-react): add react frontend container

--- init.sh
+++ init.sh
@@ -7,6 +7,8 @@ cd "$(dirname "$0")"
 if [[ $1 == "-f" ]]; then
   echo "Forcing a clean install…"
   rm -rf ../sl-data
+  rm -rf ../sl-server
+  rm -rf ../sl-react
   rm -rf ../sl-lambda
 fi
 
@@ -16,6 +18,10 @@ fi
 
 if [[ ! -d ../sl-server ]]; then
   git clone --branch develop git@github.com:adriancarriger/sl-server.git ../sl-server
+fi
+
+if [[ ! -d ../sl-react ]]; then
+  git clone --branch develop git@github.com:adriancarriger/sl-react.git ../sl-react
 fi
 
 if [[ ! -d ../sl-lambda ]]; then
